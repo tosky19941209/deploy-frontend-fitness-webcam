68f2d45085d6b893e8218829d69cd94bae275bce
felixeliass1994 <felixeliass1994@gmail.com>
2024-02-19 03:42:48 +1000

    2_step_build

--- src/style/config/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    baseclass : "d-flex flex-column justify-content-center align-items-md-center",
-    name: 'App',
-    components: {
-        signin : "signin",
-        signup : "signup"   
-    }
-}
